1a97790ddc1d291922d231e056ee4b722058d63c
Jack Knoell <j.knoell@edenspiekermann.com>
2024-04-24 17:20:26 -0700

    add redirect for homepage in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+      },
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+};
 
 export default nextConfig;
