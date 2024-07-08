40ff851a1eaade70b7fb32e7f21f9a31b804ae6e
Jack Knoell <j.knoell@edenspiekermann.com>
2024-07-08 12:38:14 -0700

    feat: header case study & case study page implementation

---
+++ src/sections/header-case-study/interfaces.ts
@@ -0,0 +1,9 @@
+import { MediaProperties } from "@/components/media/interfaces";
+
+export interface HeaderCaseStudyProperties {
+  title: string;
+  subtitle: string;
+  featuredMedia: MediaProperties;
+  backgroundColor: string;
+  inverted: boolean;
+}
