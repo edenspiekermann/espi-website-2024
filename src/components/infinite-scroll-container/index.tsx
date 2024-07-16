7188c8083301dbb482713b1148bafc9bf835822b
Jack Knoell <j.knoell@edenspiekermann.com>
2024-07-16 15:12:36 -0700

    simplify infinite scroll container component

--- src/components/infinite-scroll-container/index.tsx
+++ src/components/infinite-scroll-container/index.tsx
@@ -1,80 +1,48 @@
-import { motion, useMotionValue } from "framer-motion";
-import React, { useEffect, useState, forwardRef } from "react";
+import { motion, useAnimation } from "framer-motion";
+import React, { useEffect, useState, useRef } from "react";
+import styles from "./styles.module.scss";
 
 interface InfiniteScrollContainerProps {
   children?: React.ReactNode;
-  className?: string;
 }
 
-export const InfiniteScrollContainer = forwardRef<
-  HTMLDivElement,
-  InfiniteScrollContainerProps
->(({ children, className }, ref) => {
-  const x = useMotionValue(0);
-  const [sliderWidth, setSliderWidth] = useState(0);
+export const InfiniteScrollContainer = ({
+  children,
+}: InfiniteScrollContainerProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [containerWidth, setContainerWidth] = useState(0);
+  const controls = useAnimation();
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (ref && "current" in ref && ref.current !== null) {
-        const sliderContainer = ref.current as HTMLDivElement;
-        if (sliderContainer.scrollLeft <= 0) {
-          sliderContainer.scrollLeft = sliderContainer.scrollWidth / 2;
-        } else if (
-          sliderContainer.scrollLeft + sliderContainer.clientWidth >=
-          sliderContainer.scrollWidth
-        ) {
-          sliderContainer.scrollLeft =
-            sliderContainer.scrollWidth / 2 - sliderContainer.clientWidth;
-        }
-      }
-    };
-
-    if (ref && "current" in ref && ref.current !== null) {
-      const sliderContainer = ref.current as HTMLDivElement;
-      setSliderWidth(sliderContainer.scrollWidth);
-      sliderContainer.addEventListener("scroll", handleScroll);
-
-      return () => {
-        sliderContainer.removeEventListener("scroll", handleScroll);
-      };
+    if (containerRef.current) {
+      setContainerWidth(containerRef.current.getBoundingClientRect().width);
+      controls.start({
+        x: -containerWidth,
+        transition: { duration: 15, repeat: Infinity, ease: "linear" },
+      });
     }
-  }, [children, ref]);
-
-  const handleDragEnd = (
-    sliderContainerRef: React.RefObject<HTMLDivElement>,
-  ) => {
-    if (sliderContainerRef.current !== null) {
-      const sliderContainer = sliderContainerRef.current;
-      if (sliderContainer.scrollLeft <= 0) {
-        sliderContainer.scrollLeft = sliderContainer.scrollWidth / 2;
-      } else if (
-        sliderContainer.scrollLeft + sliderContainer.clientWidth >=
-        sliderContainer.scrollWidth
-      ) {
-        sliderContainer.scrollLeft =
-          sliderContainer.scrollWidth / 2 - sliderContainer.clientWidth;
-      }
-    }
-  };
-
-  const dragConstraints = {
-    left: sliderWidth ? -sliderWidth / 2 : 0,
-    right: 0,
-  };
+  }, [containerRef, controls, containerWidth]);
 
   return (
     <motion.div
-      className={className}
-      ref={ref}
-      style={{ x }}
-      drag="x"
-      dragConstraints={dragConstraints}
-      onDragEnd={() => handleDragEnd(ref as React.RefObject<HTMLDivElement>)}
+      className={styles.carouselContainer}
+      ref={containerRef}
+      onMouseEnter={() => controls.stop()}
+      onMouseLeave={() =>
+        controls.start({
+          x: -containerWidth,
+          transition: { duration: 15, repeat: Infinity, ease: "linear" },
+        })
+      }
     >
-      {children}
-      {children}
+      <motion.div className={styles.items} animate={controls}>
+        {children}
+      </motion.div>
+      <motion.div className={styles.items} animate={controls}>
+        {children}
+      </motion.div>
     </motion.div>
   );
-});
+};
 
 InfiniteScrollContainer.displayName = "InfiniteScrollContainer";
--- src/sections/slider-news/index.tsx
+++ src/sections/slider-news/index.tsx
@@ -2,7 +2,7 @@
 
 import { Divider } from "@/components/divider/divider";
 import { SliderNewsFragment } from "@/graphql/types/generated";
-import React, { useRef } from "react";
+import React from "react";
 import { ArticleCard } from "./article-card";
 import classNames from "classnames";
 import styles from "./styles.module.scss";
@@ -18,16 +18,11 @@ export const SliderNews = ({
     container: true,
   });
 
-  const sliderContainerRef = useRef<HTMLDivElement>(null);
-
   return (
     <section>
       {showDivider && divider && <Divider {...divider} />}
       <div className={containerClass}>
-        <InfiniteScrollContainer
-          ref={sliderContainerRef}
-          className={styles.sliderContainer}
-        >
+        <InfiniteScrollContainer>
           {articles?.map((article) => (
             <ArticleCard key={article.id} {...article} />
           ))}
