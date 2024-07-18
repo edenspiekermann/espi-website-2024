426581e653ad463b75802f3abb8bcfffdf15e5f0
Jack Knoell <j.knoell@edenspiekermann.com>
2024-07-18 10:50:25 -0700

    remove unused imports

--- src/components/filter/filter-news.tsx
+++ src/components/filter/filter-news.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./styles.module.scss";
-import { CloseFilter } from "./close-filter";
 import classNames from "classnames";
 
 interface FilterProperties {
