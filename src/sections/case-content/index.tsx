6a66feb59ba610f700b11c22043549b2a31cf9bd
Jack Knoell <j.knoell@edenspiekermann.com>
2024-07-19 14:02:24 -0700

    initial implementation of case content section component

---
+++ src/sections/case-content/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import { CaseContentFragment } from "@/graphql/types/generated";
+import { Media } from "@/components/media";
+import { MediaProperties } from "@/components/media/interfaces";
+import classNames from "classnames";
+import { Divider } from "@/components/divider/divider";
+import { StructuredText } from "react-datocms";
+
+export const CaseContent = ({
+  headlineText,
+  bodyText,
+  content,
+}: CaseContentFragment) => {
+  const { blocks } = content;
+  const caseContentClass = classNames({
+    [styles.caseContent]: true,
+    container: true,
+  });
+
+  const renderBlock = ({ record }: { record: any }) => {
+    switch (record.__typename) {
+      case "CaseStudyContentImageRecord":
+        return (
+          <div
+            key={record.id}
+            className={record.halfSize ? styles.halfImage : styles.fullImage}
+          >
+            <Media {...(record.media as MediaProperties)} />
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <>
+      <Divider invertColor={false} />
+      <section className={caseContentClass}>
+        <div className={styles.left}>
+          <div className={styles.text}>
+            <p className={styles.headlineText}>{headlineText}</p>
+            {bodyText && (
+              <div
+                dangerouslySetInnerHTML={{ __html: bodyText }}
+                className={styles.bodyText}
+              />
+            )}
+          </div>
+        </div>
+        <div className={styles.right}>
+          <StructuredText data={content} renderBlock={renderBlock} />
+        </div>
+      </section>
+    </>
+  );
+};
