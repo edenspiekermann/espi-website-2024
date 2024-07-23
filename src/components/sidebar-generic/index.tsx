ad97a2722578a9fa1f444c18e4b0e7ca1f1459d8
Jack Knoell <j.knoell@edenspiekermann.com>
2024-07-22 21:12:21 -0700

    implement the sidebar generic sub component

---
+++ src/components/sidebar-generic/index.tsx
@@ -0,0 +1,23 @@
+import { SidebarGenericFragment } from "@/graphql/types/generated";
+import classNames from "classnames";
+import React from "react";
+import styles from "./styles.module.scss";
+import { Button } from "../button/button";
+
+export const SidebarGeneric = ({
+  text,
+  textStyle,
+  callToAction,
+}: SidebarGenericFragment) => {
+  const textClass = classNames({
+    [styles.text]: true,
+    [styles[textStyle!]]: true,
+  });
+
+  return (
+    <div className={styles.sidebarGeneric}>
+      <div className={textClass} dangerouslySetInnerHTML={{ __html: text }} />
+      {callToAction && <Button {...callToAction} />}
+    </div>
+  );
+};
