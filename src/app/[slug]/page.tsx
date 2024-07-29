3b3b669eb2e7d4477342fb06bacb4b0333304375
Jack Knoell <j.knoell@edenspiekermann.com>
2024-07-29 15:03:39 -0700

    implement 404 page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,27 +3,29 @@ import { SectionConductor } from "../../sections/conductor";
 import { request } from "../../lib/request";
 import UpdateNavState from "@/components/navigation/update-nav-state";
 import { CookieConsentForm } from "@/components/cookie-consent-form";
+import { notFound } from "next/navigation";
+import Head from "next/head";
 
 type PageProps = { params: { slug: string } };
-
-export async function generateMetadata({ params }: PageProps) {
-  const capitalizedSlug =
-    params.slug.charAt(0).toUpperCase() + params.slug.slice(1);
-  return {
-    title: capitalizedSlug,
-  };
-}
 
 export default async function Page({ params }: PageProps) {
   const { page } = await request(PageDocument, {
     slug: params.slug,
   });
+
+  if (!page) {
+    notFound();
+  }
+
   const { title, sections, invertNavColor } = page!;
 
   const { cookieConsentForm } = await request(CookieConsentDocument);
 
   return (
     <>
+      <Head>
+        <title>{title || "Not Found"}</title>
+      </Head>
       <UpdateNavState isInverted={invertNavColor} />
       <SectionConductor sections={sections} />
       {cookieConsentForm && <CookieConsentForm {...cookieConsentForm} />}
