fd62e3a50ae19c3e61cd356b9912d222a0e54896
Jack Knoell <j.knoell@edenspiekermann.com>
2024-09-25 15:34:49 -0700

    feat: apply card animations to teaser related cases

--- src/sections/teaser-related-case/index.tsx
+++ src/sections/teaser-related-case/index.tsx
@@ -1,27 +1,35 @@
+"use client";
+
 import {
   CaseStudyGridCardRelatedCaseFragment,
   TeaserRelatedCaseFragment,
 } from "@/graphql/types/generated";
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./styles.module.scss";
 import { Divider } from "@/components/divider/divider";
 import { CaseStudyRelatedCard } from "./card";
+import { useInView } from "framer-motion";
 
 export const TeaserRelatedCase = ({
   showDivider,
   divider,
   caseStudies,
 }: TeaserRelatedCaseFragment) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.5 });
+
   return (
     <section>
       {showDivider && divider && <Divider {...divider} />}
-      <div className={`${[styles.teaserRelatedCase]} container`}>
+      <div className={`${[styles.teaserRelatedCase]} container`} ref={ref}>
         {caseStudies?.map(
-          (caseStudy) =>
+          (caseStudy, index) =>
             caseStudy.__typename === "CaseStudyRecord" && (
               <CaseStudyRelatedCard
                 key={caseStudy.id}
                 {...(caseStudy as CaseStudyGridCardRelatedCaseFragment)}
+                index={index}
+                isInView={isInView}
               />
             ),
         )}
