0f5cca55551f468591def3a3b33ab9aacaa22264
Jack Knoell <j.knoell@edenspiekermann.com>
2024-10-02 12:52:57 -0700

    feat: make tags filter to work page from case studies

--- src/sections/case-intro/index.tsx
+++ src/sections/case-intro/index.tsx
@@ -5,6 +5,7 @@ import { CaseIntroFragment } from "@/graphql/types/generated";
 import React, { useContext } from "react";
 import styles from "./styles.module.scss";
 import { CaseStudyContext } from "@/context/case-study-context";
+import Link from "next/link";
 
 export const CaseIntro = ({
   text,
@@ -15,11 +16,35 @@ export const CaseIntro = ({
   const { relatedIndustries, relatedServices } = caseStudyState;
   const tags = [...relatedIndustries, ...relatedServices];
 
+  const getTagFilter = (tag: string) => {
+    return {
+      tag: tag.replace(/\s/g, "-"),
+    };
+  };
+
   return (
     <section className={styles.caseIntro}>
       <div className="container">
         <div className={styles.left}>
-          {tags?.map((tag) => <Tag key={tag} text={tag} isLargeTag />)}
+          {tags?.map((tag) => (
+            <Link
+              key={tag}
+              href={{
+                pathname: "/work",
+                query: {
+                  filter: relatedIndustries.includes(tag)
+                    ? "industries"
+                    : relatedServices.includes(tag)
+                      ? "services"
+                      : null,
+                  ...getTagFilter(tag),
+                },
+              }}
+              className={styles.tag}
+            >
+              <Tag key={tag} text={tag} isLargeTag />
+            </Link>
+          ))}
         </div>
         <div className={styles.right}>
           <p className={styles.text}>{text}</p>
