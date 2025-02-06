a6e5c7b3db0d3e44f66b2a0b2bff2088c1746d94
Jack Knoell <j.knoell@edenspiekermann.com>
2025-02-05 16:42:39 -0800

    feat: add localization on home and work pages

---
+++ src/app/[slug]/[locale]/page.tsx
@@ -0,0 +1,57 @@
+import {
+  CookieConsentDocument,
+  LocalizedPageDocument,
+} from "@/graphql/types/generated";
+import { SectionConductor } from "../../../sections/conductor";
+import { request } from "../../../lib/request";
+import UpdateNavState from "@/components/navigation/update-nav-state";
+import { CookieConsentForm } from "@/components/cookie-consent-form";
+import { notFound } from "next/navigation";
+import Head from "next/head";
+
+type PageProps = { params: { slug: string; locale: string } };
+
+export async function generateMetadata({ params }: PageProps) {
+  const capitalizedSlug = params.slug
+    .split("-")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+  return {
+    title: capitalizedSlug,
+  };
+}
+
+export default async function Page({ params }: PageProps) {
+  const { page } = await request(LocalizedPageDocument, {
+    slug: params.slug,
+    region: params.locale,
+  });
+
+  if (!page) {
+    notFound();
+  }
+
+  const { title, sections, invertNavColor } = page!;
+
+  const { cookieConsentForm } = await request(CookieConsentDocument);
+
+  return (
+    <>
+      <Head>
+        <title>{title || "Not Found"}</title>
+      </Head>
+      <UpdateNavState isInverted={invertNavColor} />
+      <SectionConductor
+        sections={sections}
+        indexToFadeIn={title === "News" ? 2 : 1}
+        style={{
+          backgroundColor:
+            title === "News"
+              ? "var(--color-extra-light-grey)"
+              : "var(--color-white)",
+        }}
+      />
+      {cookieConsentForm && <CookieConsentForm {...cookieConsentForm} />}
+    </>
+  );
+}
